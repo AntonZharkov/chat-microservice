9372ce8fa39cba02aa52b3d29c315cea7a81454e
AntonZharkov <19antoni88@gmail.com>
2023-11-04 18:04:47 +0300

    add user to channel group when new message event

--- web/chat/consumers.py
+++ web/chat/consumers.py
@@ -1,11 +1,18 @@
 import json
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
-from typing import TypedDict, NamedTuple
+from base64 import b64decode
 from dataclasses import dataclass
-from api.v1.chat.services import ChatQueryService
 from enum import Enum
-from .models import Message
+from typing import NamedTuple, TypedDict
+
+from asgiref.sync import sync_to_async
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from django.conf import settings
+
+from api.v1.chat.services import ChatQueryService
+
+from .models import Message
+from main.services import RedisUserStatus
+
 
 class ReceivedData(TypedDict):
     command: str
@@ -16,10 +23,13 @@ class ActionType(Enum):
     ADD = 1
     DISCARD = -1
 
+
 class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def new_message_handler(self, data: dict):
         chat_id = data['chat_id']
         data['user'] = self.user
+        if file := data.get('file'):
+            decoded_file = b64decode(file['content'])
         message: Message = await Message.objects.acreate(author=self.user['id'], chat_id=chat_id, body=data['body'])
         data['created'] = str(message.created)
 
@@ -28,9 +38,8 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             {
                 'type': 'new_message_event',
                 'data': data,
-            }
+            },
         )
-
 
     async def write_message_handler(self, data: dict):
         chat_id = data['chat_id']
@@ -39,10 +48,17 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             {
                 'type': 'write_message_event',
                 'data': data,
-            }
+            },
         )
 
     async def new_chat_event(self, event: dict):
+        # TODO: когда создается новый чат добавляем юзера к в группу этого чата чтобы ему приходили ивенты
+        redis_service = RedisUserStatus()
+        status_online = redis_service.get_status(self.user['id'])['online']
+        if status_online:
+            chat_id = event['data']['chat_id']
+            await self.channel_layer.group_add(chat_id, self.channel_name)
+
         await self.send_json(event)
 
     async def new_message_event(self, event: dict):
@@ -54,14 +70,12 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def user_online_event(self, event: dict):
         await self.send_json(event)
 
-
     commands = {
         'new_message': new_message_handler,
         'write_message': write_message_handler,
     }
 
     async def update_chat_groups(self, action: ActionType):
-
         service = ChatQueryService()
         chats = service.get_chats(self.user['id'])
 
@@ -72,36 +86,41 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
             async for chat in chats:
                 await self.channel_layer.group_add(chat.id, self.channel_name)
                 await self.channel_layer.group_send(
-                            chat.id,
-                            {
-                                'type': 'user_online_event',
-                                'data': {
-                                    'user_name': self.user['full_name'],
-                                    'online': True,
-                                    'chat_id': chat.id,
-                                }
-                            }
+                    chat.id,
+                    {
+                        'type': 'user_online_event',
+                        'data': {
+                            'user_name': self.user['full_name'],
+                            'online': True,
+                            'chat_id': chat.id,
+                        },
+                    },
                 )
         elif action == ActionType.DISCARD:
             async for chat in chats:
                 await self.channel_layer.group_discard(chat.id, self.channel_name)
                 await self.channel_layer.group_send(
-                            chat.id,
-                            {
-                                'type': 'user_online_event',
-                                'data': {
-                                    'online': False,
-                                    'chat_id': chat.id,
-                                }
-                            }
-                        )
+                    chat.id,
+                    {
+                        'type': 'user_online_event',
+                        'data': {
+                            'online': False,
+                            'chat_id': chat.id,
+                        },
+                    },
+                )
 
     async def connect(self):
         self.user: dict = self.scope['user']
+        service = RedisUserStatus()
+        await sync_to_async(service.set_online)(self.user['id'])
+
         await self.update_chat_groups(ActionType.ADD)
         await self.accept()
 
     async def disconnect(self, close_code):
+        service = RedisUserStatus()
+        await sync_to_async(service.set_offline)(self.user['id'])
         await self.update_chat_groups(ActionType.DISCARD)
 
     # Receive message from WebSocket
