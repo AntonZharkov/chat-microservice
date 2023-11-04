45dd27914194a56cade5b8081822b8ffe7785c16
AntonZharkov <19antoni88@gmail.com>
2023-11-04 18:02:02 +0300

    add no_avatar

--- web/api/v1/chat/serializers.py
+++ web/api/v1/chat/serializers.py
@@ -1,6 +1,8 @@
-from rest_framework import serializers
-from chat.models import Chat, Message, UserChat
 from django.core.cache import cache
+from rest_framework import serializers
+
+from chat.models import Chat, Message, UserChat
+
 from main.utils import find_dict_in_list
 
 
@@ -11,7 +13,7 @@ class InitSerializer(serializers.Serializer):
 class UserChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserChat
-        fields = ('user', )
+        fields = ('user',)
 
 
 class ChatListSerializer(serializers.ModelSerializer):
@@ -26,21 +28,38 @@ class ChatListSerializer(serializers.ModelSerializer):
         representation = super().to_representation(instance)
         user_id = representation.pop('user_chats')[0]['user']
         user_info = find_dict_in_list(self.context.get('user_data'), 'id', user_id)
+        user_status = find_dict_in_list(self.context.get('users_status'), 'id', user_id)
 
         if user_info:
             # TODO: добавил аватарку и на блоге тоже в user_list.js
             avatar_name = user_info['avatar'].split('/')[-1]
-            user_info['avatar'] = 'http://localhost:8000/media/no_ava/no_ava.png' if avatar_name == 'no-image-available.jpg' else user_info['avatar']
-            representation.update({
-                'avatar': user_info['avatar'],
-                'user_id': user_info['id'],
-            })
+            user_info['avatar'] = (
+                'http://localhost:8000/media/no_ava/no_ava.png'
+                if avatar_name == 'no-image-available.jpg'
+                else user_info['avatar']
+            )
+            representation.update(
+                {
+                    'avatar': user_info['avatar'],
+                    'user_id': user_info['id'],
+                    'status': {
+                        'online': user_status['online'],
+                        'date': user_status['date'],
+                    },
+                }
+            )
         else:
-            representation.update({
-                'name': 'Unknown user',
-                'avatar': 'http://localhost:8000/media/no_ava/deleted.jpg',
-                'user_id': user_id,
-            })
+            representation.update(
+                {
+                    'name': 'Unknown user',
+                    'avatar': 'http://localhost:8000/media/no_ava/deleted.jpg',
+                    'user_id': user_id,
+                    'status': {
+                        'online': False,
+                        'date': None,
+                    },
+                }
+            )
 
         return representation
 
